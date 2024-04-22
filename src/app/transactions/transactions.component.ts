f8e400db1ccd2d16cc9bc471c7acb236fa159e42
makepool <makepool@hotmail.com>
2024-04-22 18:25:41 +0100

    Add pagination.

--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import { tap } from 'rxjs';
+import { BehaviorSubject, tap } from 'rxjs';
 import { TransationsApiService } from './transations-api.service';
 import { TableComponent } from '../components/table/table.component';
 import { Transaction } from './transactions.model';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-transactions',
@@ -10,25 +12,39 @@ import { Transaction } from './transactions.model';
   templateUrl: './transactions.component.html',
   styleUrl: './transactions.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [TableComponent],
+  imports: [NgIf, AsyncPipe, TableComponent, MatPaginatorModule],
 })
 export class TransactionsComponent {
+  private readonly pageIndex$ = new BehaviorSubject(0);
+
+  readonly pageIndex = signal(this.pageIndex$);
+
+  readonly totalResults = signal(0);
+
   readonly tableData = signal<Transaction[]>([]);
 
   readonly loading = signal(true);
 
-  constructor(api: TransationsApiService) {
-    api
-      .get()
-      .pipe(
-        tap({
-          next: (result) => {
-            console.log('result', result);
-            this.tableData.set(result.items);
-            this.loading.set(false);
-          },
-        })
-      )
-      .subscribe();
+  readonly paginationAndFilter$ = this.pageIndex$.pipe(
+    tap((pageIndex) => {
+      this.api
+        .get(pageIndex)
+        .pipe(
+          tap({
+            next: (result) => {
+              this.totalResults.set(result.totalNumberOfItems);
+              this.tableData.set(result.items);
+              this.loading.set(false);
+            },
+          })
+        )
+        .subscribe();
+    })
+  );
+
+  constructor(private api: TransationsApiService) {}
+
+  pageChanged(data: PageEvent) {
+    this.pageIndex$.next(data.pageIndex);
   }
 }
