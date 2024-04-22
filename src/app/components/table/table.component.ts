d7fd6d3b5c385bba7239e95c9e3fcf8108d19620
makepool <makepool@hotmail.com>
2024-04-22 18:18:19 +0100

    Add loading spinner from Angular Material.

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Transaction } from '../../transactions/transactions.model';
 import { DatePipe, DecimalPipe, NgFor, NgIf } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'vyne-table',
   standalone: true,
-  imports: [NgFor, NgIf, DatePipe, DecimalPipe],
+  imports: [NgFor, NgIf, DatePipe, DecimalPipe, MatProgressSpinnerModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
